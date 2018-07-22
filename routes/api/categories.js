59fd18745aa08b12ffffb208ed193d704f092e07
viktimolog <viktimolog@gmail.com>
2018-07-22 12:00:34 +0300

    addCategory is working

--- routes/api/categories.js
+++ routes/api/categories.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
-// const gravatar = require('gravatar');
+const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+
+const Category = require('../../models/Category');
 
 // @route   GET api/categories/test
 // @desc    Tests categories route
@@ -9,5 +11,43 @@ const bcrypt = require('bcryptjs');
 router.get('/test', (req, res) => res.json({
     msg: "Categories works"
 }));
+
+// @route  POST api/categories/addCategory
+// @desc   addCategory
+// @access Public
+router.post('/addCategory', (req, res) => {
+    Category.findOne({
+        name: req.body.name,
+        rating: req.body.rating,
+        parent: req.body.parent,
+        child: req.body.child
+    })
+        .then(item => {
+            if (item) {
+                res.json({
+                    success: false
+                });
+            } else {
+                const newCategory = new Category({
+                    name: req.body.name,
+                    rating: req.body.rating,
+                    parent: req.body.parent,
+                    child: req.body.child
+                });
+                newCategory.save()
+                    .then(item => {
+                        if (item) {
+                            res.json({
+                                success: true,
+                                category: item
+                            });
+                        } else {
+                            return res.status(400).json({ msg: 'Error on the server! Category has not added' });
+                        }
+                    })
+                    .catch(err => console.log(err));
+            }
+        })
+})
 
 module.exports = router;
